fc6d4e8eb746a80c8dfb163301e2151cea9c0142
David Jarrin <dmjarrin@gmail.com>
2021-03-04 17:40:14 -0500

    Initial Commit

---
+++ iterable-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iterable-demo
+//
+//  Created by David Jarrin on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
